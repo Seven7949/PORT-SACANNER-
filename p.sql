810ee789e10b99d15c65dd830c7f8051ff402ada
Seven7949 <scissorseven7949@gmail.com>
2025-04-20 14:48:09 +0530

    p.sql

---
+++ p.sql
@@ -0,0 +1,6 @@
+🔍 Scanning scanme.nmap.org for open ports...
+
+[OPEN] Port 22 - Banner: SSH-2.0-OpenSSH_7.6p1 Ubuntu-4ubuntu0.3
+[OPEN] Port 80 - Banner: HTTP/1.1 400 Bad Request
+
+⏱️ Scan completed in 0:00:04.891531
